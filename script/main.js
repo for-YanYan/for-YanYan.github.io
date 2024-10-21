3b5bdbcfff549299589ebebfe160bdeb60b6ac26
for-YanYan <245982960@qq.com>
2024-10-20 21:51:16 -0400

    Update main.js

--- script/main.js
+++ script/main.js
@@ -102,7 +102,7 @@ const animationTimeline = () => {
     )
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
+    .from(".idea-2", 10.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
